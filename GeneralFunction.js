006f29cd582983f7d6485a477575543103565344
Freda Wong <freda.wolfkiller@hotmail.com>
2022-08-04 20:15:26 +0800

    update alert color

--- GeneralFunction.js
+++ GeneralFunction.js
@@ -38,15 +38,36 @@ function updateBtn(mode, version){
 	}
 }
 function showAlert(type, title, content){
-	var icon;
+	var icon, color;
 	if(type == "alert"){
 		icon = "bi-exclamation-diamond-fill";
+		color = "red";
+		$("#alertDiv").addClass("alertMode");
+		
 	}else if(type == "question"){
 		icon = "bi-question-diamond-fill";
+		color = "yellow";
+		$("#alertDiv").addClass("questionMode");
 	}else{
 		icon = "bi-bookmark-star-fill"; 
+		color = "blue";
+		$("#alertDiv").addClass("remarkMode");
 	}
-	$("#alertTitle").html("<i class='bi " + icon + " w3-text-red'></i>" + title);
+	$("#alertTitle").html(	"<div class='w3-display-container w3-center w3-col-middle w3-pale-" + color + " w3-leftbar w3-rightbar w3-border-" + color + " w3-large'>" + 
+							"	<i class='bi " + icon + " w3-text-" + color + "'></i>" + title + 
+							"</div>");
 	$("#alertContent").html(content);
+	if(type == "question"){
+		$("#alertAction").html(	"<button class='wolf-btn w3-btn w3-round-large w3-pale-" + color + " w3-border-" + color + " wolf-margin-x' onclick='closeHiddenPopupInfo()'>" + 
+									"確認" + 
+								"</button>" + 
+								"<button class='wolf-btn w3-btn w3-round-large w3-pale-" + color + " w3-border-" + color + " wolf-margin-x' onclick='closeHiddenPopupInfo()'>" + 
+									"取消" + 
+								"</button>");
+	}else{
+		$("#alertAction").html(	"<button class='wolf-btn w3-btn w3-round-large w3-pale-" + color + " w3-border-" + color + " wolf-margin-x' onclick='closeHiddenPopupInfo()'>" + 
+									"關閉" + 
+								"</button>");
+	}
 	$("#alertDiv").css("display","block");
 }
